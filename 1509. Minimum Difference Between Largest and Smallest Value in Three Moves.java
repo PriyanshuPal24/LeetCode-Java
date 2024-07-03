ba38a66678098dab5ad53660dd9b5e2f19493434
PriyanshuPal24 <135883761+PriyanshuPal24@users.noreply.github.com>
2024-07-03 17:16:07 +0530

    Create 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.java

---
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minDifference(int[] nums) {
+        int n=nums.length;
+        if(n<=4){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int res=Integer.MAX_VALUE;
+
+        res=Math.min(res,nums[n-4]-nums[0]);
+        res=Math.min(res,nums[n-3]-nums[1]);
+        res=Math.min(res,nums[n-2]-nums[2]);
+        res=Math.min(res,nums[n-1]-nums[3]);
+        return res;
+
+    }
+}
