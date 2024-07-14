6b32698f9f4e0a0418dc9c205bbde1835837213b
PriyanshuPal24 <135883761+PriyanshuPal24@users.noreply.github.com>
2024-07-14 20:46:18 +0530

    Create 51. N-Queens.java

---
+++ 51. N-Queens.java
@@ -0,0 +1,54 @@
+class Solution {
+    public void backtrack(List<List<String>> solutions, char[][] board, int row, int n) {
+        if (row == n) {
+            solutions.add(construct(board));
+            return;
+        }
+        
+        for (int col = 0; col < n; col++) {
+            if (isValid(board, row, col, n)) {
+                board[row][col] = 'Q';
+                backtrack(solutions, board, row + 1, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    public boolean isValid(char[][] board, int row, int col, int n) {
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public List<String> construct(char[][] board) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> solutions = new ArrayList<>();
+        char[][] board = new char[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        
+        backtrack(solutions, board, 0, n);
+        return solutions;
+    }
+}
